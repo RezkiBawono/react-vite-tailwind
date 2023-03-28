13edcec8c0f35e19db483dc5cd4cba4d0ab5d4a6
RezkiBawono <rezkibawono@gmail.com>
2023-03-28 14:14:17 +0700

    Delete postcss.config replace with original docs.

--- template/postcss.config.cjs
+++
@@ -1,9 +0,0 @@
-const { join } = require("path");
-const tailwindcss = require("tailwindcss");
-
-module.exports = {
-  plugins: [
-    tailwindcss(join(__dirname, "tailwind.config.js")),
-    require("autoprefixer"),
-  ],
-};
